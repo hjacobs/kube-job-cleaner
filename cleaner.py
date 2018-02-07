70a433f3a7a2462f770808a0ef25c499072360cc
Alexey Ermakov <alexey.ermakov@zalando.de>
2018-02-07 12:44:06 +0100

    Refactor: extract stuff to functions

--- cleaner.py
+++ cleaner.py
@@ -11,34 +11,16 @@ def parse_time(s: str):
     return datetime.datetime.strptime(s, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=datetime.timezone.utc).timestamp()
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--seconds', type=int, default=3600, help='Delete all finished jobs older than ..')
-parser.add_argument('--timeout-seconds', type=int, default=-1, help='Kill all jobs older than ..')
-parser.add_argument('--dry-run', action='store_true', help='Dry run mode')
-args = parser.parse_args()
-
-try:
-    config = pykube.KubeConfig.from_service_account()
-except FileNotFoundError:
-    # local testing
-    config = pykube.KubeConfig.from_file(os.path.expanduser('~/.kube/config'))
-api = pykube.HTTPClient(config)
-
-now = time.time()
-for job in pykube.Job.objects(api, namespace=pykube.all):
+def job_expired(max_age, timeout_seconds, job):
+    now = time.time()
     completion_time = job.obj['status'].get('completionTime')
     status = job.obj['status']
 
     if (status.get('succeeded') or status.get('failed')) and completion_time:
         completion_time = parse_time(completion_time)
         seconds_since_completion = now - completion_time
-        if seconds_since_completion > args.seconds:
-            print('Deleting {} ({:.0f}s old)..'.format(job.name, seconds_since_completion))
-            if args.dry_run:
-                print('** DRY RUN **')
-            else:
-                job.delete()
-            continue
+        if seconds_since_completion > max_age:
+            return '{:.0f}s old'.format(seconds_since_completion)
 
     start_time = job.obj['status'].get('startTime')
     if start_time:
@@ -46,25 +28,20 @@ for job in pykube.Job.objects(api, namespace=pykube.all):
 
         # Determine the timeout in seconds for this job
         annotations = job.obj['metadata'].get('annotations', {})
-        cleanup_timeout = int(annotations.get('cleanup-timeout', args.timeout_seconds))
+        cleanup_timeout = int(annotations.get('cleanup-timeout', timeout_seconds))
 
-        # Check whether a timeout is active for this job.
-        if cleanup_timeout < 0:
-            continue
+        if cleanup_timeout > 0 and seconds_since_start > cleanup_timeout:
+            return 'timeout ({:.0f}s running)'.format(seconds_since_start)
 
-        if start_time and seconds_since_start > cleanup_timeout:
-            print('Deleting Job because of timeout {} ({:.0f}s running)..'.format(job.name, seconds_since_start))
-            if args.dry_run:
-                print('** DRY RUN **')
-            else:
-                job.delete()
 
-for pod in pykube.Pod.objects(api, namespace=pykube.all):
+def pod_expired(max_age, pod):
+    now = time.time()
     if pod.obj['status'].get('phase') in ('Succeeded', 'Failed'):
         seconds_since_completion = 0
         if pod.obj['status'].get('containerStatuses') is None:
             print("Warning: Skipping pod without containers ({})".format(pod.obj['metadata'].get('name')))
-            continue
+            return
+
         for container in pod.obj['status'].get('containerStatuses'):
             if 'terminated' in container['state']:
                 state = container['state']
@@ -78,9 +55,40 @@ for pod in pykube.Pod.objects(api, namespace=pykube.all):
                 if seconds_since_completion == 0 or finish < seconds_since_completion:
                     seconds_since_completion = finish
 
-        if seconds_since_completion > args.seconds:
-            print('Deleting {} ({:.0f}s old)..'.format(pod.name, seconds_since_completion))
-            if args.dry_run:
-                print('** DRY RUN **')
-            else:
-                pod.delete()
+        if seconds_since_completion > max_age:
+            return '{:.0f}s old'.format(seconds_since_completion)
+
+
+def delete_if_expired(dry_run, entity, reason):
+    if reason:
+        print("Deleting {} {} ({})".format(entity.kind, entity.name, reason))
+        if dry_run:
+            print('** DRY RUN **')
+        else:
+            entity.delete()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--seconds', type=int, default=3600, help='Delete all finished jobs older than ..')
+    parser.add_argument('--timeout-seconds', type=int, default=-1, help='Kill all jobs older than ..')
+    parser.add_argument('--dry-run', action='store_true', help='Dry run mode')
+    args = parser.parse_args()
+
+    try:
+        config = pykube.KubeConfig.from_service_account()
+    except FileNotFoundError:
+        # local testing
+        config = pykube.KubeConfig.from_file(os.path.expanduser('~/.kube/config'))
+    api = pykube.HTTPClient(config)
+
+    for job in pykube.Job.objects(api, namespace=pykube.all):
+        delete_if_expired(args.dry_run, job, job_expired(args.seconds, args.timeout_seconds, job))
+
+    for pod in pykube.Pod.objects(api, namespace=pykube.all):
+        delete_if_expired(args.dry_run, pod, pod_expired(args.seconds, pod))
+
+
+if __name__ == "__main__":
+    main()
+
